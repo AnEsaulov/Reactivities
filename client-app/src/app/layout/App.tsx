b95ac5f4157d6ba08af892be063ed6217a44ce04
AnEsaulov <an.esaulov@gmail.com>
2021-12-23 11:55:04 +0200

    End of section 10

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,12 +7,17 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <NavBar />
       <Container style={{marginTop: '7em'}}> 
         <Routes>
@@ -21,6 +26,9 @@ function App() {
           <Route path='/activities/:id' element={<ActivityDetails />} />
           <Route key={location.key} path='/createActivity' element={<ActivityForm />} />
           <Route key={location.key} path='/manage/:id' element={<ActivityForm />} />
+          <Route path='/errors' element={<TestErrors />} />
+          <Route path='/server-error' element={<ServerError />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Container>
     </>
