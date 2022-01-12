ada4b25f63184451f126da4bbdaea31f648fba86
AnEsaulov <an.esaulov@gmail.com>
2022-01-12 18:29:55 +0200

    End of section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
@@ -21,8 +21,8 @@ namespace API.Controllers
             
         }
 
-
-         [HttpGet("{id}")]
+        //[Authorize]
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetActivity(Guid id) 
         {
             return HandleResult(await Mediator.Send(new Details.Query{Id = id}));
--- API/Startup.cs
+++ API/Startup.cs
@@ -19,6 +19,8 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using FluentValidation.AspNetCore;
 using API.Middleware;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace API
 {
@@ -35,11 +37,17 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers().AddFluentValidation(config =>
+            services.AddControllers(opt =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                opt.Filters.Add(new AuthorizeFilter(policy));
+            })
+                .AddFluentValidation(config =>
             {
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
             services.AddApplicationServices(Configuration);
+            services.AddIdentityServices(Configuration);
 
         }
 
@@ -60,6 +68,7 @@ namespace API
 
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
