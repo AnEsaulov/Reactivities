4eb35cbe127639dc34f4e78bc8010a3adddbde2b
AnEsaulov <an.esaulov@gmail.com>
2022-02-08 11:32:09 +0200

    End of section 18

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Profiles;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -13,6 +15,12 @@ namespace API.Controllers
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
