688d12a0b93f32a45bbdcf2a4bc3e8fa2b3b894f
AnEsaulov <an.esaulov@gmail.com>
2022-02-22 17:43:37 +0200

    refresh token complete

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -72,6 +72,7 @@ namespace API.Extensions
                         .AllowAnyMethod()
                         .AllowAnyHeader()
                         .AllowCredentials()
+                        .WithExposedHeaders("WWW-Authenticate", "Pagination")
                         .WithOrigins("https://localhost:3000");
                 });
             });
